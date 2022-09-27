2fc377657b6666f75915d216673eb1f06813fc35
Nahyun Kim <nahyun@sookmyung.ac.kr>
2022-09-27 17:03:09 +0900

    04_4(2)

--- main.c
+++ main.c
@@ -10,9 +10,10 @@ int main(int argc, char *argv[]) {
 	printf("input the year: ");
 	scanf("%d", &year);
 	
-	
-	printf("is the year %d the leap year? :%d", year, year%4 == 0 && year%100 != 0 || year%400 == 0);
-	
+	if ( year%4 == 0 && year%100 != 0 || year%400 == 0 )
+		printf("%d is leap year!\n", year);
+	else 
+		printf("%d is not leap year!\n", year);
 
 	return 0;
 }
